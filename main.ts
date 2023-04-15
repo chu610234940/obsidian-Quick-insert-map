4009a5b69b519e65427e38ef9d3d1a1b33b50a5a
chu610234940 <67134208+chu610234940@users.noreply.github.com>
2023-04-15 22:28:37 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,126 @@
-import { App, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
-import { MapView } from './main';
+import {
+  App, 
+  Modal, 
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
+  WorkspaceLeaf,
+  MarkdownView,
+  MarkdownPostProcessorContext
+} from 'obsidian';
 
-class MapView extends obsidian.VueComponent {
-    leafletMap: L.Map;
-
-    constructor(app: obsidian.App, private containerEl: HTMLElement) {
-        super(app);
-
-        const mapEl = this.containerEl.createEl('div', { cls: 'my-plugin-map' });
-        this.leafletMap = L.map(mapEl).setView([51.505, -0.09], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        }).addTo(this.leafletMap);
-    }
-
-    getMapView(): HTMLElement {
-        return this.containerEl;
-    }
+interface MyPluginSettings {
+  mySetting: string;
 }
 
-export default class MyPlugin extends obsidian.Plugin {
-    async onload() {
-        console.log('loading Quick insert map plugin');
+const DEFAULT_SETTINGS: MyPluginSettings = {
+  mySetting: 'default'
+}
 
-        this.addCommand({
-    id: 'insert-map',
-    name: 'Insert Map',
-    callback: () => {
-        this.insertMapView();
-    },
-    hotkeys: [
-        {
-            modifiers: ['Mod'],
-            key: 'm',
-        },
-    ],
-});
+export default class MyPlugin extends Plugin {
+  settings: MyPluginSettings;
 
+  async onload() {
+    console.log('Loading plugin');
+
+    await this.loadSettings();
+
+    this.addCommand({
+      id: 'insert-map',
+      name: 'Insert Map',
+      callback: this.insertMap
+    });
+
+    this.addSettingTab(new MyPluginSettingsTab(this.app, this));
+
+    console.log('Plugin loaded');
+  }
+
+  async onunload() {
+    console.log('Unloading plugin');
+
+    console.log('Plugin unloaded');
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  async insertMap() {
+    const leaf = this.app.workspace.activeLeaf;
+    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+    if (!leaf || !view) {
+      new Notice('No markdown view available');
+      return;
     }
 
-    onunload() {
-        console.log('unloading Quick insert map plugin');
+    const editor = view.sourceMode.cmEditor;
+
+    const cursor = editor.getCursor();
+    const line = editor.getLine(cursor.line);
+    const match = line.match(/^\s*-\s+(\[\[.*\]\])/);
+
+    if (!match) {
+      new Notice('No link under cursor');
+      return;
     }
+
+    const [, link] = match;
+
+    const file = this.app.metadataCache.getFirstLinkpathDest(link, this.app.workspace.getActiveFile().path);
+
+    if (!file) {
+      new Notice('Link target not found');
+      return;
+    }
+
+    const map = await this.createMap(file);
+
+    editor.replaceRange(`\n\n<div class="map">\n\n${map}\n\n</div>\n\n`, {
+      line: cursor.line + 1,
+      ch: 0
+    });
+  }
+
+  async createMap(file: TFile): Promise<string> {
+    const mapUrl = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s+555555(${encodeURIComponent(`${file.name}`)})/${encodeURIComponent(`${file.dir}/${file.name}`)},${encodeURIComponent(`${file.name}`)}/500x300?access_token=${this.settings.mySetting}`;
+
+    return `![${file.name}](${mapUrl})`;
+  }
+}
+
+class MyPluginSettingsTab extends PluginSettingTab {
+  plugin: MyPlugin;
+
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let {containerEl} = this;
+
+    containerEl.empty();
+
+    containerEl.createEl('h2', {text: 'Settings for Quick Insert Map'});
+
+    new Setting(containerEl)
+      .setName('Mapbox Access Token')
+      .setDesc('Enter your Mapbox access token. Get one at https://account.mapbox.com/access-tokens/')
+      .addText(text => text
+        .setPlaceholder('Enter access token')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+
+  }
 }
